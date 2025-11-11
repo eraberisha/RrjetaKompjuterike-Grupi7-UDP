dc7ed83b45a295742be5da87ab0d9e220b39d41d
Erblin Syla <erblin.sylaa@gmail.com>
2025-11-11 20:35:18 +0100

    Client tracking

--- server.c
+++ server.c
@@ -26,3 +26,34 @@ int find_client(struct sockaddr_in *addr, socklen_t addr_len) {
     }
     return -1;
 }
+
+int add_client(struct sockaddr_in *addr, socklen_t addr_len, uint32_t client_id, int is_admin) {
+    if (client_count >= MAX_CLIENTS) {
+        printf("ERROR: Max clients reached (%d)\n", MAX_CLIENTS);
+        return -1;
+    }
+
+    int idx = client_count++;
+    clients[idx].addr = *addr;
+    clients[idx].addr_len = addr_len;
+    clients[idx].client_id = client_id;
+    clients[idx].is_admin = is_admin;
+    clients[idx].last_seen = time(NULL);
+    clients[idx].msg_count = 0;
+    clients[idx].bytes_in = 0;
+    clients[idx].bytes_out = 0;
+    clients[idx].active = 1;
+
+    char ip_str[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &addr->sin_addr, ip_str, INET_ADDRSTRLEN);
+    printf("NEW CLIENT: ID=%u | IP=%s:%d | Admin=%s\n",
+           client_id, ip_str, ntohs(addr->sin_port), is_admin ? "YES" : "NO");
+
+    return idx;
+}
+
+void update_client(int idx, int bytes_received) {
+    clients[idx].last_seen = time(NULL);
+    clients[idx].msg_count++;
+    clients[idx].bytes_in += bytes_received;
+}
