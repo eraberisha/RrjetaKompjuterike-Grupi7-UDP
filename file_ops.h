78511f4e9b03e6fbfee9ce8970607316c9f798a0
eraberisha <era.berisha5@student.uni-pr.edu>
2025-11-14 13:38:05 +0100

    Perditesim dhe ristrukturim

--- file_ops.h
+++ file_ops.h
@@ -1,16 +1,24 @@
-// Ky fajll eshte header per modulin "file_ops.c, deklaron funksionet per menaxhimin 
+// Ky fajll eshte header per modulin "file_ops.c", deklaron funksionet per menaxhimin 
 // e fajllave dhe komandave qe vijne nga klientet."
  #ifndef FILE_OPS_H
  #define FILE_OPS_H 
  
- #include <stdio.h> 
- 
- void list_files(char *response); 
- void read_file(const char *filename, char *response); 
- void file_info(const char *filename, char *response); 
- int delete_file(const char *filename); 
- int search_file(const char *keyword, char *response); 
- int upload_file(const char *filename, const char *data); 
- int download_file(const char *filename, char *response); 
+ #include <stdbool.h>
+
+ #define CHUNK_SIZE 1024
+ #define FILE_FOLDER "./server_files/"
+
+//Funksionet per komanda 
+ void list_files(int client_sock);
+ void read_file(int client_sock, char *filename);
+ void file_info(int client_sock, char *filename); 
+ void search_files(int client_sock, char *text);
+
+ //Upload dhe download
+ void handle_upload(int client_sock, char *filename, bool is_admin);
+ void handle_download(int client_sock, char *filename);
+
+ //Delete
+ void delete_file(int client_sock, char *filename, bool is_admin);
  
  #endif
